15030fa15510857d9d975fb109eb4e6368ababec
BMRicardas <Ricardas.Brazdzius@gmail.com>
2022-05-18 16:47:30 +0300

    add start screen animation

--- src/components/StartingScreen/StartingScreen.tsx
+++ src/components/StartingScreen/StartingScreen.tsx
@@ -4,7 +4,11 @@ import classes from "./StartingScreen.module.scss";
 const StartingScreen: FC = () => {
   return (
     <div className={classes["starting-screen"]}>
-      <div></div>
+      <div className={classes["starting-screen__dots-container"]}>
+        <div className={classes["starting-screen__dot"]}></div>
+        <div className={classes["starting-screen__dot"]}></div>
+        <div className={classes["starting-screen__dot"]}></div>
+      </div>
       <p className={classes["starting-screen__text"]}>
         Press any key on your keyboard to start
       </p>
